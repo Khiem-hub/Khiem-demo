f6c9c20842f4870a69e9fb909536ec21c0aabb12
Khiem-hub <andreynguyen1112@gmail.com>
2026-01-13 11:34:25 +0700

    them file

--- demo.py
+++ demo.py
@@ -1,2 +1,16 @@
-print("Hello, world!")
-print("This is a demo Python script.")
+try:
+    hours = float(input("Enter Hours: "))
+    rate = float(input("Enter Rate: "))
+except ValueError:
+    print("Error, please enter numeric input")
+    raise SystemExit
+
+if hours > 40:
+    regular_pay = 40 * rate
+    overtime_hours = hours - 40
+    overtime_pay = overtime_hours * rate * 1.5
+    pay = regular_pay + overtime_pay
+else:
+    pay = hours * rate
+
+print("Pay:", pay)
